10e94b9ffd459338fccad3691c3b0ffa7b66885a
Bonolo Kenton Gaosenkwe <bonolo.kamolane@gmail.com>
2023-07-31 13:22:39 +0200

    Added docstrings on enoid_app/apps.py

--- enoid_app/apps.py
+++ enoid_app/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class EnoidAppConfig(AppConfig):
+    """
+    Represents the configuration for the Enoid app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'enoid_app'
+
