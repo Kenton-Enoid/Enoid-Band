7f4af8a9222daa5d590de96d32698c0aa21b305e
Bonolo Kenton Gaosenkwe <bonolo.kamolane@gmail.com>
2023-07-31 13:23:43 +0200

    Added docstrings on enoid_app/forms.py

--- enoid_app/forms.py
+++ enoid_app/forms.py
@@ -2,6 +2,9 @@ from django.forms import ModelForm
 from .models import Message
 
 class MessageForm(ModelForm):
+    """
+    Represents a form for creating a new message.
+    """
     class Meta:
         model = Message
         fields = ['name', 'email', 'message']
