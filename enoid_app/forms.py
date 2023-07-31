60e76794f7c30be72202ea3f969da95e5a37533b
Bonolo Kenton Gaosenkwe <bonolo.kamolane@gmail.com>
2023-07-31 13:37:23 +0200

    Made few changes on enoid_app

--- enoid_app/forms.py
+++ enoid_app/forms.py
@@ -2,9 +2,7 @@ from django.forms import ModelForm
 from .models import Message
 
 class MessageForm(ModelForm):
-    """
-    Represents a form for creating a new message.
-    """
+    
     class Meta:
         model = Message
         fields = ['name', 'email', 'message']
